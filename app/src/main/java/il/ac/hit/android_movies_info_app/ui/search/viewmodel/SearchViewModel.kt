aa9af1b04a53de2a905224d7e570e31448f641b4
orelrafa <orelrafailov1998@gmail.com>
2024-07-15 12:37:40 +0300

    Implemented the search feature.

---
+++ app/src/main/java/il/ac/hit/android_movies_info_app/ui/search/viewmodel/SearchViewModel.kt
@@ -0,0 +1,29 @@
+package il.ac.hit.android_movies_info_app.ui.search.viewmodel
+
+import android.util.Log
+import androidx.constraintlayout.widget.ConstraintSet.Transform
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
+import com.bumptech.glide.load.Transformation
+import dagger.hilt.android.lifecycle.HiltViewModel
+import il.ac.hit.android_movies_info_app.data.model.movie_search.Movie
+import il.ac.hit.android_movies_info_app.data.repositories.movie_repository.MovieRepository
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchViewModel @Inject constructor(
+    movieRepository: MovieRepository
+):ViewModel() {
+    private val _query = MutableLiveData<String>()
+
+    //God bless this man, Transformations has become deprecated.
+    //https://stackoverflow.com/questions/75465435/unresolved-reference-transformations-after-upgrading-lifecycle-dependency/75465436#75465436
+    val movies = _query.switchMap(){
+        movieRepository.getSearchedMovies(it)
+    }
+
+    fun setQuery(query : String){
+        _query.value = query
+    }
+}
