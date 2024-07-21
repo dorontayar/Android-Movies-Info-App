32113c4e943eef86863b6d285ad3332920f2e8ec
RedMecrucy0 <doront2010@gmail.com>
2024-07-21 18:49:16 +0300

    change language change alert dialog

--- app/src/main/java/il/ac/hit/android_movies_info_app/ui/MainActivity.kt
+++ app/src/main/java/il/ac/hit/android_movies_info_app/ui/MainActivity.kt
@@ -17,28 +17,57 @@ import java.util.Locale
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var previousLocale: Locale
+    private lateinit var previousLanguage: String
+    private var previousOrientation: Int = Configuration.ORIENTATION_UNDEFINED
+    private var restartDialog: AlertDialog? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        previousLocale = Locale.getDefault()
+        previousLanguage = Locale.getDefault().language
+        previousOrientation = resources.configuration.orientation
     }
+
+
+
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
-        val currentLocale = newConfig.locales[0]
-        if (currentLocale != previousLocale) {
-            previousLocale = currentLocale
-            showRestartDialog(this)
+
+        val currentOrientation = newConfig.orientation
+        if (currentOrientation != previousOrientation) {
+            previousOrientation = currentOrientation
+            if (restartDialog?.isShowing == true) {
+
+                val currentLanguage = newConfig.locales[0].language
+                if (currentLanguage != previousLanguage) {
+                    previousLanguage = currentLanguage
+                    showRestartDialog(this)
+                }
+            }
+
+        }else{
+            val currentLanguage = newConfig.locales[0].language
+            if (currentLanguage != previousLanguage) {
+                previousLanguage = currentLanguage
+                showRestartDialog(this)
+            }
         }
     }
 
     private fun showRestartDialog(context: Context) {
-        AlertDialog.Builder(context)
-            .setTitle(getString(R.string.language_change))
-            .setCancelable(false)
-            .setMessage(getString(R.string.to_apply_changes_please_restart_the_app))
-            .setPositiveButton(getString(R.string.restart)) { _, _ ->
-                ProcessPhoenix.triggerRebirth(context)
-            }
-            .show()
+        if (restartDialog?.isShowing == true) {
+            restartDialog?.dismiss()
+            restartDialog = null
+        }
+        else {
+            restartDialog = AlertDialog.Builder(context)
+                .setTitle(getString(R.string.language_change))
+                .setCancelable(false)
+                .setMessage(getString(R.string.to_apply_changes_please_restart_the_app))
+                .setPositiveButton(getString(R.string.restart)) { _, _ ->
+                    ProcessPhoenix.triggerRebirth(context)
+                }
+                .show()
+        }
     }
 }
