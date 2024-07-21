4578951a05e45163cece253fee37a7ce1c9d64a2
Nikita Borochov <nikitaboro03@gmail.com>
2024-07-21 12:40:34 +0300

    hebrew api

--- app/src/main/java/il/ac/hit/android_movies_info_app/data/remote_db/MovieRemoteDataSource.kt
+++ app/src/main/java/il/ac/hit/android_movies_info_app/data/remote_db/MovieRemoteDataSource.kt
@@ -1,5 +1,6 @@
 package il.ac.hit.android_movies_info_app.data.remote_db
 
+import il.ac.hit.android_movies_info_app.utils.getApiLanguage
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -8,12 +9,15 @@ class MovieRemoteDataSource @Inject constructor(
     private val movieService:MovieService
 ) :BaseDataSource()
 {
-    suspend fun searchMovie(query: String) = getResult { movieService.searchMovie(query) }
-    suspend fun searchMovieScrolling(query: String,page:Int) = getResult { movieService.searchMovieScrolling(query,page) }
-    suspend fun getMovieDetails(id: Int) = getResult { movieService.getMovieDetails(id) }
-    suspend fun getTopRatedMovies() = getResult { movieService.getTopRatedMovies() }
-    suspend fun getUpcomingMovies() = getResult { movieService.getUpcomingMovies() }
+
+    private val language = getApiLanguage()
+
+    suspend fun searchMovie(query: String) = getResult { movieService.searchMovie(query,language=language) }
+    suspend fun searchMovieScrolling(query: String,page:Int) = getResult { movieService.searchMovieScrolling(query,page,language=language) }
+    suspend fun getMovieDetails(id: Int) = getResult { movieService.getMovieDetails(id,language=language) }
+    suspend fun getTopRatedMovies() = getResult { movieService.getTopRatedMovies(language=language) }
+    suspend fun getUpcomingMovies() = getResult { movieService.getUpcomingMovies(language=language) }
 
     suspend fun getUpcomingMoviesByDateRange(startDate: String, endDate: String) = getResult {
-        movieService.getUpcomingMoviesByDateRange(startDate.toString(), endDate.toString()) }
+        movieService.getUpcomingMoviesByDateRange(startDate.toString(), endDate.toString(),language=language) }
 }
